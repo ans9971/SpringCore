cb82ff582484c0d332c75df8bae2414ff11d50b5
ans9971 <ans9971@naver.com>
2022-08-16 23:58:47 +0900

    .

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -6,5 +6,11 @@ public class Member {
     private String name;
     private Gradle grade;
 
+    public Member(long id, String name, Gradle grade){
+        this.id = id;
+        this.name=name;
+        this.grade=grade;
+    }
+
 
 }
